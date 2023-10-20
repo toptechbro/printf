2a6a86efd8c4f0c07a90e212b383419ccd14bc4b
Samuel Monye <toptechbro@yahoo.com>
2023-10-20 14:46:27 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,45 +1,86 @@
 #include "main.h"
+int print_str(const char *str);
+int len_str(const char *str);
+int _printf(const char *format, ...);
 /**
- * _printf - A function that selects the correct item to print.
- * @format: identifier to check for.
- * Return: the length of the printed string.
- */
-int _printf(const char * const format, ...)
+*print_char - Prints character to standard output.
+*@char_index: Working character.
+*Return: Integer.
+*/
+int print_char(char char_index)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
+return (write(1, &char_index, 1));
+}
 
-	va_list args;
-	int i = 0, j, len = 0;
+/**
+*len_str - Takes length of string.
+*@str: Working string.
+*Return: Size of the string.
+*/
+int len_str(const char *str)
+{
+int size;
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+for (size = 0; str[size] != '\0'; size++)
+{
+/* This does not print anything*/
+}
+return (size);
+}
 
-Here:
-	while (format[i] != '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
-	}
-	va_end(args);
-	return (len);
+/**
+*print_str - Prints string to standard output.
+*@str: This is my working string.
+*Return: All characters.
+*/
+int print_str(const char *str)
+{
+return (write(1, str, len_str(str)));
+}
+
+/**
+*_printf - Custom printf.
+*@format: This is my format string.
+*Return: Integer.
+*/
+int _printf(const char *format, ...)
+{
+int times;
+char char_index;
+char *str;
+va_list arg_p;
+
+times = 0;
+va_start(arg_p, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 's':
+str = va_arg(arg_p, char *);
+times += print_str(str);
+break;
+case 'c':
+char_index = va_arg(arg_p, int);
+times += print_char(char_index);
+break;
+case '%':
+times += print_char('%');
+break;
+default:
+times += print_char('%');
+times += print_char(*format);
+}
+}
+else
+{
+times += print_char(*format);
+}
+format++;
+}
+va_end(arg_p);
+return (times);
 }
